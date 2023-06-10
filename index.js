57b044f7b494196c5825a5777890d8ccec911ab3
marufhossain511 <mfmarufh@gmail.com>
2023-06-10 21:13:57 +0600

    create verify jwt token and send to client side

--- index.js
+++ index.js
@@ -4,11 +4,31 @@ const cors = require('cors');
 const app =express()
 const stripe=require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors())
 app.use(express.json())
+
+const verifyJWT=(req,res,next)=>{
+  const authorization=req.headers.authorization
+  if(!authorization){
+    return res.status(401).send({error:true,message:'unauthorized access'})
+  }
+  const token=authorization.split(' ')[1]
+  if(!token){
+    return res.status(401).send({error:true,message:'unauthorized access'})
+  }
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(error,decoded)=>{
+    if(error){
+      return res.status(401).send({error:true,message:'unauthorized access'})
+    }
+    req.decoded=decoded
+    next()
+  })
+}
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vvlmqn2.mongodb.net/?retryWrites=true&w=majority`;
@@ -35,6 +55,13 @@ async function run() {
     const paymentCollection = client.db("summerSchoolDB").collection("payments");
     const enrolledClassCollection = client.db("summerSchoolDB").collection("enrolledClass");
 
+
+    // jwt
+    app.post('/jwt',(req,res)=>{
+      const user=req.body
+      const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:"1h"})
+      res.send({token})
+    })
 
     // payment-intent
     app.post("/create-payment-intent", async (req, res) => {
@@ -174,17 +201,7 @@ async function run() {
         res.send(result)
     })
 
-    // app.patch('/classseats/:id',async(req,res)=>{
-    //   const id =req.params.id
-    //   const query={_id: new ObjectId(id)}
-    //   const course=await classCollection.findOne(query)
-    //   const updateDoc={
-    //     $set:{
-    //       availableSeat:course.availableSeat -1
-    //     }
-    //   }
-    //   const result=
-    // })
+  
 
     app.patch('/classes/:id',async(req,res)=>{
       const id = req.params.id
@@ -277,6 +294,11 @@ async function run() {
       // console.log(email);
       const query={instructorEmail:email}
       const result=await pendingClassCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    app.get('/pendingClasses',async(req,res)=>{
+      const result = await pendingClassCollection.find().toArray()
       res.send(result)
     })
 
